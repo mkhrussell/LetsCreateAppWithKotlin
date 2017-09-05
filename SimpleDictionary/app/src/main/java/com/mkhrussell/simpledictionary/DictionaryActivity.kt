479889552fac918f85601b91e5da7f314dc81a4f
Mohammad Kamrul Hasan <mkhrussell@gmail.com>
2017-09-05 20:10:11 +0600

    Let's Create App Using Kotlin #01 - Dictionary App Part #02 - ActionBar and Search
    https://www.youtube.com/watch?v=15qtmx1dXjk

---
+++ SimpleDictionary/app/src/main/java/com/mkhrussell/simpledictionary/DictionaryActivity.kt
@@ -0,0 +1,57 @@
+package com.mkhrussell.simpledictionary
+
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.support.v7.widget.SearchView
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+
+class DictionaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dictionary)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        if(intent?.action.equals(Intent.ACTION_SEARCH)) {
+            val searchQuery = intent?.getStringExtra(SearchManager.QUERY) ?: ""
+            Log.d("DictionaryActivity", "searchQuery = $searchQuery")
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_dictionary, menu)
+
+        val searchView: SearchView? = menu.findItem(R.id.action_search) as? SearchView
+        val searchManager: SearchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView?.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView?.isSubmitButtonEnabled = true
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_search -> {
+                onSearchRequested()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
